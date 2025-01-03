0a407edf32234516f5dfba88b61ae838b7a43a77
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-01-03 16:02:04 +0100

    Set up build and release workflows (#12)
    * update workflows
    
    * update dependencies
    
    * update go version in action
    
    * remove arm64 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 self_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 root_dir=$(readlink -f "$self_dir/..")
 
-version=$1
+version=${1:-$(git rev-parse --abbrev-ref HEAD)}
 short_hash=$(git rev-parse --short HEAD)
 
 go build -o "$root_dir/build/knit" -ldflags \
-    "-X 'knit/pkg/util.Version=$version' -X 'knit/pkg/util.ShortHash=$short_hash' -X 'knit/pkg/util.Architecture=${GOARCH:-amd64}'" \
+    "-w -s -X 'knit/pkg/util.Version=$version' -X 'knit/pkg/util.ShortHash=$short_hash' -X 'knit/pkg/util.Architecture=${GOARCH:-amd64}'" \
     "$root_dir/main.go" 
